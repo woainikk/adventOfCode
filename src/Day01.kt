dc06fb37a78d929308d61f562e2003696cad0305
Margarita Bobova <margarita.bobova@jetbrains.com>
2022-12-01 12:38:50 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
+
+    fun calculateSnacks(input: List<String>): MutableList<Int>{
+        val reindeers: MutableList<Int> = mutableListOf()
+        var sum = 0
+        for (i in input) {
+            if(i.isEmpty()){
+                reindeers.add(sum)
+                sum = 0
+                continue
+            }
+            else{
+                sum += i.toInt()
+            }
+        }
+        return reindeers
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calculateSnacks(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculateSnacks(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
